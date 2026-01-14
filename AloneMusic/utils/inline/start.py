3f5fcd693e9e8c9a258cf6ede4c820f55c2d5aa0
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2026-01-14 23:29:39 +0530

    Update inline keyboard buttons in start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -33,13 +33,13 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="shiv_aarumi"),
+            InlineKeyboardButton(
+                text="💌 ʏᴛ-ᴀᴘɪ",
+                callback_data="bot_info_data"
+            ),
+        ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
     ]
     return buttons
