d6702b48a85c5b2d59f04ddec794e7a49e1a00c1
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2026-01-23 02:03:12 +0530

    Update restart.py

--- AloneMusic/plugins/sudo/restart.py
+++ AloneMusic/plugins/sudo/restart.py
@@ -5,7 +5,6 @@
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
 # All rights reserved.
-
 import asyncio
 import os
 import shutil
@@ -20,125 +19,151 @@ from pyrogram import filters
 import config
 from AloneMusic import app
 from AloneMusic.misc import HAPP, SUDOERS, XCB
-from AloneMusic.utils.database import (get_active_chats, remove_active_chat,
-                                       remove_active_video_chat)
+from AloneMusic.utils.database import (
+    get_active_chats,
+    remove_active_chat,
+    remove_active_video_chat,
+)
 from AloneMusic.utils.decorators.language import language
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 async def is_heroku():
-    return "heroku" in socket.getfqdn()
+    return "heroku" in socket.getfqdn().lower()
 
 
+# ---------------- LOGS ----------------
 @app.on_message(filters.command(["getlog", "logs", "getlogs"]) & SUDOERS)
 @language
 async def log_(client, message, _):
     try:
-        await message.reply_document(document="log.txt")
+        await message.reply_document("log.txt")
     except:
         await message.reply_text(_["server_1"])
 
 
+# ---------------- UPDATE ----------------
 @app.on_message(filters.command(["update", "gitpull"]) & SUDOERS)
 @language
 async def update_(client, message, _):
     if await is_heroku():
         if HAPP is None:
             return await message.reply_text(_["server_2"])
+
     response = await message.reply_text(_["server_3"])
+
     try:
-        repo = Repo()
+        repo = Repo(search_parent_directories=True)
     except GitCommandError:
         return await response.edit(_["server_4"])
     except InvalidGitRepositoryError:
         return await response.edit(_["server_5"])
-    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
-    os.system(to_exc)
-    await asyncio.sleep(7)
+
+    os.system(f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null")
+    await asyncio.sleep(5)
+
     verification = ""
-    REPO_ = repo.remotes.origin.url.split(".git")[0]
-    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        verification = str(checks.count())
+    for check in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        verification = str(check.count())
+
     if verification == "":
         return await response.edit(_["server_6"])
-    updates = ""
 
-    def ordinal(format):
+    updates = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[0]
+
+    def ordinal(n):
         return "%d%s" % (
-            format,
-            "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+            n,
+            "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10 :: 4],
         )
 
     for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
-    _final_updates_ = _update_response_ + updates
-    if len(_final_updates_) > 4096:
-        url = await AnonyBin(updates)
-        nrs = await response.edit(
-            f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
+        date = datetime.fromtimestamp(info.committed_date)
+        updates += (
+            f"<b>➣ #{info.count()}: "
+            f"<a href={REPO_}/commit/{info}>{info.summary}</a> "
+            f"ʙʏ -> {info.author}</b>\n"
+            f"\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> "
+            f"{ordinal(int(date.strftime('%d')))} "
+            f"{date.strftime('%b')}, {date.strftime('%Y')}\n\n"
         )
-    else:
-        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
-    os.system("git stash &> /dev/null && git pull")
+
+    final_text = (
+        "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n"
+        "➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n"
+        "<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
+        f"{updates}"
+    )
+
+    await response.edit(final_text, disable_web_page_preview=True)
+
+    os.system("git stash &> /dev/null && git pull &> /dev/null")
 
     try:
-        served_chats = await get_active_chats()
-        for x in served_chats:
+        chats = await get_active_chats()
+        for x in chats:
             try:
                 await app.send_message(
-                    chat_id=int(x),
-                    text=_["server_8"].format(app.mention),
+                    int(x),
+                    _["server_8"].format(app.mention),
                 )
                 await remove_active_chat(x)
                 await remove_active_video_chat(x)
             except:
                 pass
-        await response.edit(f"{nrs.text}\n\n{_['server_7']}")
+
+        await response.edit(f"{final_text}\n\n{_['server_7']}")
     except:
         pass
 
+    # -------- RESTART --------
     if await is_heroku():
         try:
             os.system(
-                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
+                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}"
+                f"{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}"
+                f"{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}"
+                f"{XCB[10]}{XCB[2]}{XCB[5]} "
+                f"{XCB[11]}{XCB[4]}{XCB[12]}"
             )
             return
         except Exception as err:
-            await response.edit(f"{nrs.text}\n\n{_['server_9']}")
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=_["server_10"].format(err),
+            await app.send_message(
+                config.LOGGER_ID,
+                _["server_10"].format(err),
             )
     else:
-        os.system("pip3 install .")
-        os.system(f"kill -9 {os.getpid()} && bash start")
-        exit()
+        os.system("uv pip install -e .")
+        os.system(f"kill -9 {os.getpid()} && python3 -m AloneMusic")
 
 
+# ---------------- RESTART ----------------
 @app.on_message(filters.command(["restart"]) & SUDOERS)
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
-    ac_chats = await get_active_chats()
-    for x in ac_chats:
+
+    chats = await get_active_chats()
+    for x in chats:
         try:
             await app.send_message(
-                chat_id=int(x),
-                text=f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
+                int(x),
+                f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\n"
+                "ʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ "
+                "ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
             )
             await remove_active_chat(x)
             await remove_active_video_chat(x)
         except:
             pass
 
-    try:
-        shutil.rmtree("downloads")
-        shutil.rmtree("raw_files")
-        shutil.rmtree("cache")
-    except:
-        pass
-    await response.edit_text(
-        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
+    for folder in ("downloads", "raw_files", "cache"):
+        if os.path.exists(folder):
+            shutil.rmtree(folder, ignore_errors=True)
+
+    await response.edit(
+        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, "
+        "ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs..."
     )
-    os.system(f"kill -9 {os.getpid()} && bash start")
+    os.system(f"kill -9 {os.getpid()} && python3 -m AloneMusic")
