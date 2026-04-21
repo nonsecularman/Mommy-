c997965101c8a3d2ecefd23cca490abbb7bf3042
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2026-04-21 14:39:24 +0530

    Update import path for assistants in broadcast.py

--- AloneMusic/plugins/misc/broadcast.py
+++ AloneMusic/plugins/misc/broadcast.py
@@ -159,7 +159,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from Alone.core.userbot import assistants
+        from AloneMusic.core.userbot import assistants
 
         for num in assistants:
             sent = 0
